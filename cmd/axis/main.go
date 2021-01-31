52db08115255aacce5958644995306d51963ecb1
Mattias Kindborg @FantasticFiasco <FantasticFiasco@users.noreply.github.com>
2021-01-31 09:31:39 +0100

    feat(action): version (#2)
    Add support for calling CLI with command "--version".

---
+++ cmd/axis/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/FantasticFiasco/axis-cli/internal/build"
+)
+
+func main() {
+	version := flag.Bool("version", false, "Show axis version")
+	flag.Parse()
+
+	if *version == true {
+		fmt.Printf("axis version %s (%s)\n", build.Version, build.Date)
+		fmt.Println(build.ReleaseURL)
+	}
+}
---
+++ internal/build/build.go
@@ -0,0 +1,5 @@
+package build
+
+var Version string = "development"
+var ReleaseURL string = "https://github.com/FantasticFiasco/axis-cli/releases"
+var Date string = "YYYY-MM-DD"
