76ebdcddd0cfabbe31324af983ec0644469a94c6
FantasticFiasco <mattias.kindborg@gmail.com>
2021-02-05 23:40:46 +0100

    wip

--- scripts/releasenotes/main.go
+++ scripts/releasenotes/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"os"
-	"regexp"
-	"strings"
 )
 
 func main() {
@@ -26,52 +22,4 @@ func main() {
 	}
 
 	fmt.Print(releaseNotes)
-}
-
-func ReadFromFile(filename, version string) (string, error) {
-	matched, err := regexp.MatchString(`^\d+.\d+.\d+$`, version)
-	if err != nil {
-		return "", err
-	}
-	if !matched {
-		return "", fmt.Errorf(`"%s" does not conform to the expected version format`, version)
-	}
-
-	file, err := os.Open(filename)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	return readChapterContent(file, version)
-}
-
-func readChapterContent(r io.Reader, version string) (string, error) {
-	startPrefix := fmt.Sprintf("## [%s]", version)
-	stopPrefix := "## "
-
-	isWithinChapter := false
-	chapterContent := ""
-
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		if strings.HasPrefix(scanner.Text(), startPrefix) {
-			isWithinChapter = true
-		} else if isWithinChapter && strings.HasPrefix(scanner.Text(), stopPrefix) {
-			break
-		} else if isWithinChapter {
-			chapterContent += scanner.Text() + "\n"
-		}
-	}
-
-	if scanner.Err() != nil {
-		return "", scanner.Err()
-	}
-
-	chapterContent = strings.Trim(chapterContent, "\n")
-	if chapterContent == "" {
-		return "", fmt.Errorf("release with version %s was not found in changelog", version)
-	}
-
-	return chapterContent, nil
 }
