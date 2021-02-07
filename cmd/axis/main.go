bd0660b24171e51c48e43e985c7f785b0473f2f8
FantasticFiasco <mattias.kindborg@gmail.com>
2021-02-07 23:21:57 +0100

    wip

--- cmd/axis/main.go
+++ cmd/axis/main.go
@@ -1,34 +1,30 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"github.com/FantasticFiasco/axis-cli/cmd/axis/commands"
-	"os"
-)
-
 // The following variables are set by goreleaser during CD
 var version = "<version>"
 var commit = "<git sha>"
 var date = "<date>"
 
 func main() {
-	versionFlag := flag.Bool("version", false, "Show axis version")
-	searchFlag := flag.Bool("search", false, "Search for devices on the network")
-	flag.Parse()
+	// TODO: Create basic configuration of the CLI parser
+	// TODO: Re-implement --version (https://github.com/urfave/cli/blob/master/docs/v2/manual.md#version-flag)
 
-	var err error
+	//versionFlag := flag.Bool("version", false, "Show axis version")
+	//searchFlag := flag.Bool("search", false, "Search for devices on the network")
+	//flag.Parse()
+	//
+	//var err error
+	//
+	//if *versionFlag == true {
+	//	commands.Version(version, commit, date)
+	//} else if *searchFlag {
+	//	err = commands.Search()
+	//} else {
+	//	flag.Usage()
+	//}
 
-	if *versionFlag == true {
-		commands.Version(version, commit, date)
-	} else if *searchFlag {
-		err = commands.Search()
-	} else {
-		flag.Usage()
-	}
-
-	if err != nil {
-		fmt.Fprint(os.Stderr, err)
-		os.Exit(1)
-	}
+	//if err != nil {
+	//	fmt.Fprint(os.Stderr, err)
+	//	os.Exit(1)
+	//}
 }
